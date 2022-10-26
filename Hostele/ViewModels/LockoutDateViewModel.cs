7f6b9633c2fb886725cd3a362dac6e0c24d90b81
Grpat <pg056635@student.ath.edu.pl>
2022-10-26 19:07:53 +0200

    Lista, uzytkowników, usuwanie, blokowanie

---
+++ Hostele/ViewModels/LockoutDateViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Hostele.CustomValidators;
+
+namespace Hostele.ViewModels;
+
+public class LockoutDateViewModel
+{
+    [FutureDateValidation(ErrorMessage = "Must be future date")] 
+    public DateTime LockoutDate { get; set; }
+}
